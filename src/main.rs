7ff893f7f362cccf4d56b253b8913d7f4a4fcd08
LemonTea387 <junjielow08@gmail.com>
2025-01-01 21:50:55 +0800

    test based panic handler prints to serial.

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,20 @@ mod serial;
 mod vga;
 
 /// Function to call on panic
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
